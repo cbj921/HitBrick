d02e5674ca15bdafe9f99af442a1943aba4bd056
cbj921 <447933991@qq.com>
2019-09-02 12:06:24 +0800

    first Unity project

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float h = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+			transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+			transform.Translate(new Vector3(0, 2, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+			transform.Translate(new Vector3(0, -2, 0) * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject.Instantiate(bullet,transform.position,transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			GameObject bullet = GameObject.Instantiate(bulletPrefab,transform.position,transform.rotation);
+			Rigidbody bulletRgd = bullet.GetComponent<Rigidbody>();
+			bulletRgd.velocity = transform.forward * speed;
+		}
+	}
+}
